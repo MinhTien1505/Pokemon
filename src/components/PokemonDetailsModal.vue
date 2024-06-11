e03c563926532206b3be3077b7e3038d5dd9d756
MinhTien <tiensony1505@gmail.com>
2024-06-11 21:39:19 +0700

    commit

--- src/components/PokemonDetailsModal.vue
+++ src/components/PokemonDetailsModal.vue
@@ -116,7 +116,7 @@ onBeforeMount(() => {
   z-index: 3;
   background-color: #ffffff;
   border-radius: 8px;
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
   height: 380px;
   width: 480px;
   .pokemon-details {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import api from './api'
 
 const app = createApp(App)
 app.mount('#app')
